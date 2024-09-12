4e6482f56b4edeae52a4a127984dad36817fb574
raviraj25nov@gmail.com <raviraj25nov@gmail.com>
2024-09-12 19:56:01 +0530

    12-09-2024

--- src/test/java/com/nucleus/qa/testCases/NewProposalTestCase.java
+++ src/test/java/com/nucleus/qa/testCases/NewProposalTestCase.java
@@ -1,5 +1,40 @@
 package com.nucleus.qa.testCases;
 
-public class NewProposalTestCase {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import com.nuclesu.qa.base.TestBase;
+import com.nucleus.qa.pages.HomePage;
+import com.nucleus.qa.pages.LoginPage;
+import com.nucleus.qa.pages.NewProposalPage;
+
+public class NewProposalTestCase extends TestBase {
+
+	LoginPage loginPage;
+	HomePage homePage;
+	NewProposalPage newProposalPage;
+	
+	public NewProposalTestCase() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void SetUp() {
+		initialization();
+		loginPage = new LoginPage();
+		homePage = new HomePage();
+		newProposalPage = new NewProposalPage();
+		
+	}
+	@Test (priority = 0)
+	public void NCFPortalLoginTC() {
+		String Username = "raja.singareddy@nucleus-cf.co.uk";
+		String Password = "qatest@123";
+		Url();
+		loginPage.Login(Username, Password);
+	}
+	@Test (priority = 1)
+	public void CreateNewProposal() {
+		homePage.ClickOnNewProposal();
+	}
 }
